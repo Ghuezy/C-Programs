8ed9a5980e33042a030123c34f64494d4ff621cf
Salih Dinçay <salihdncay@gmail.com>
2022-11-30 23:19:37 +0300

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    float num1,num2,result;
+    char opt;
+
+    printf("Enter the first number: ");
+    scanf("%f",&num1);
+    printf("Enter the operator: ");
+    scanf("%s",&opt);
+    printf("Enter the second number: ");
+    scanf("%f",&num2);
+
+    if(opt == '+'){
+        result = num1 + num2;
+        printf("Result is: %.2f", result);
+    }
+    else if (opt == '-')
+    {
+        result = num1 - num2;
+        printf("Result is: %.2f", result);
+    }
+    else if (opt == '/')
+    {
+        result = num1 / num2;
+        printf("Result is: %.2f", result);
+    }
+    else if (opt == '*')
+    {
+        result = num1 * num2;
+        printf("Result is: %.2f", result);
+    }
+
+    getch();
+    return 0;
+}
